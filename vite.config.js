cb27914f6c21aa6e2903d585a6e0cbfd5c67c69e
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-11-30 17:49:52 +0200

    ghgfhf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {extensions: [".jsx", ".json", ".css", ".js"]},
   plugins: [react()],
-  base: "/sas/"
+  base: "/sas/",
 })
