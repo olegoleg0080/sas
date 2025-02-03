214688fe5bb3d68f7ea941023081d3ef1be80f06
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2025-02-03 18:41:43 +0200

    g

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/sas/", // Указываем базовый путь
-  resolve: { extensions: [".jsx", ".json", ".css", ".js"] },
-  plugins: [react()],
-  build: {
-    outDir: "dist/sas",
-    assetsDir: "assets",
-  },
-});
-
-=======
   resolve: {extensions: [".jsx", ".json", ".css", ".js"]},
   plugins: [react()],
   base: "/sas/",
@@ -21,4 +10,3 @@ export default defineConfig({
 
   
 })
->>>>>>> parent of 30a5c86 (AWS)
